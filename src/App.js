4fa56f3c3284c81e26d8508b4761a04b9e5574d7
Jeff Maxwell <maxjeffwell@gmail.com>
2018-11-12 21:23:58 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import { CreateUser } from './components/CreateUser';
+import { Login } from './components/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Code Talk</h1>
+        </header>
+        <Router>
+          <main>
+            <nav>
+              <ul className="menu">
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/createuser">Create User</Link></li>
+              </ul>
+            </nav>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/createuser" component={CreateUser} />
+          </main>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Mutations.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export const CREATE_USER = gql`
+mutation CreateUserMutation($email: String!, $password: String!){
+  signup(email: $email,password: $password)
+}`;
+
+export const LOGIN_USER = gql`
+mutation LoginMutation($email: String!, $password: String!){
+  login(email: $email, password: $password)
+}
+`;
