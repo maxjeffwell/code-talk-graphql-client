bb22bc76ea38083c4ba39a09bb652c60ce2b96c9
Jeff Maxwell <maxjeffwell@gmail.com>
2018-12-05 20:02:16 -0500

    refactored to use graphql-express instead of yoga

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,104 +1,11 @@
-import React, { Component } from 'react';
-import { Mutation, Query } from 'react-apollo';
-import { CREATE_MESSAGE } from '../Mutations';
-import { FEED_MESSAGES } from '../Queries';
-import { NEW_MESSAGE_SUBSCRIPTION} from "../Subscriptions";
-import User from './User';
+import styled from 'styled-components';
 
-import { split as SplitEditor } from 'react-ace';
-import 'brace/mode/javascript';
-import 'brace/theme/solarized_dark';
-
-export class Messages extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    }
-  }
-
-  subscribeToNewMessages(subscribeToMore) {
-    subscribeToMore({
-        document: NEW_MESSAGE_SUBSCRIPTION,
-        updateQuery:
-          (prev, {subscriptionData}) => {
-            if (!subscriptionData.data) {
-              return prev;
-            }
-            const newMessage = subscriptionData.data.newMessage;
-
-            // return Object.assign({}, prev, {
-            //   messages: [...prev.messages, newMessage]
-            // });
-
-            // es6 syntax
-
-            return {
-              ...prev,
-              messages: [...prev.messages, newMessage]
-            };
-          }
-      })
-    }
-
-  render() {
-    const { text } = this.state;
-    return (
-      <section>
-        <input type="text" placeholder="Send a message..." value={text} onChange={e => this.setState({text: e.target.value})}/>
-        <Mutation mutation={CREATE_MESSAGE} variables={{ text }} update={(store, {data: {createMessage}}) => {
-          const currentStoreState = store.readQuery({query: FEED_MESSAGES});
-          const newStoreState = [...currentStoreState.messages, createMessage];
-          store.writeQuery({
-            query: FEED_MESSAGES,
-            data: {messages: newStoreState}
-          })
-        }}>
-          {parsedLink => <button onClick={parsedLink}>Create Message</button>}
-        </Mutation>
-        <Query query={FEED_MESSAGES}>
-          {({loading, error, data, refetch, subscribeToMore}) => {
-            if (loading) return <div>Loading...</div>
-            if (error) return <div>{error.message}</div>
-            this.subscribeToNewMessages(subscribeToMore);
-            refetch();
-
-            return <div>
-              {data.messages.map((message, i) =>
-                <li key={i}>
-                  {/*<span className="sender">*/}
-
-                    {/*<User>{({ data: { currentUser } }) => { // gives us payload.data.currentUser in currentUser variable*/}
-                        {/*console.log(currentUser);*/}
-                        {/*if (currentUser) return <p>{currentUser.username}</p>;*/}
-                        {/*return null;*/}
-                      {/*}}</User>*/}
-
-
-                      {/*</span>*/}
-                  <span className="message">
-                    {message.username}: {message.text}
-                  </span>
-                </li>)}
-            </div>
-          }}
-        </Query>
-        <div className="text-editor">
-            <h4>Text Editor</h4>
-            <SplitEditor
-                mode="javascript"
-                theme="solarized_dark"
-                splits={2}
-                orientation="beside"
-                width="100%"
-                value={['hi', 'hello']}
-                fontSize="16px"
-                tabSize={2}
-                editorProps={{ $blockScrolling: true}}
-                wrapEnabled={true}
-                />
-        </div>
-        </section>
-  );
-  }
-}
+export default styled.div`
+  grid-column: 3;
+  grid-row: 2;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  flex-direction: column-reverse;
+  overflow-y: auto;
+`;
---
+++ src/graphql/team.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+export const meQuery = gql`
+    {
+        me {
+            id
+            username
+            teams {
+                id
+                name
+                admin
+                channels {
+                    id
+                    name
+                }
+            }
+        }
+    }
+`;
+
+export const idk = {};
