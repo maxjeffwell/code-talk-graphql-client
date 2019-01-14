aa2ce55403645d1c9032db14763ce6fa9b8bb3bc
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-13 21:34:42 -0500

    uris in back ticks

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import App from './components/App';
 import { signOut } from './components/SignOut';
 
 const httpLink = new HttpLink({
-	uri: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/graphql' : 'https://jmaxwell-code-talk-server.herokuapp.com/graphql'
+	uri: process.env.NODE_ENV === 'development' ? `http://localhost:8000/graphql` : `https://jmaxwell-code-talk-server.herokuapp.com/graphql`
 });
 
 const wsLink = new WebSocketLink({
