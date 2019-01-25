eb0e16e231e6e29d310fbd30139f221745cd6681
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-24 19:02:08 -0500

    move fontawesome files

--- src/components/Froala/index.js
+++ src/components/Froala/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import 'font-awesome/css/font-awesome.css';
 import FroalaEditor from 'react-froala-wysiwyg';
 
 export default class Froala extends Component {
 	render() {
-		return <FroalaEditor tag="textarea"/>;
+		return <FroalaEditor tag="textarea" />;
 	}
 }
