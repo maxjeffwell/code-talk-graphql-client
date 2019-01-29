3baf8ed64ceed2114453f5793752654c56883ad9
Jeff Maxwell <maxjeffwell@gmail.com>
2019-01-29 18:46:23 -0500

    style account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import withAuthorization from '../Session/withAuthorization';
 
+const StyledHeader = styled.h1`
+  color: ${props => props.theme.green};
+  background-color: ${props => props.theme.black};
+  border: 5px solid ${props => props.theme.green};
+  border-radius: 5px;
+  display: grid;
+  text-align: center;
+  margin: 50px auto;
+  width: 25%;
+`;
+
+const StyledP = styled.p`
+  text-align: center;
+`
+
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
+  <div role="main">
+  <section id="user-account-info" role="contentinfo">
+    <StyledHeader>ACCOUNT PAGE</StyledHeader>
+    <StyledP>Account features coming soon. Stay tuned...</StyledP>
+  </section>
   </div>
 );
 
