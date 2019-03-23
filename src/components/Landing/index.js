163d4ebc8cc467e097a66489e60a581ead93cc69
maxjeffwell <maxjeffwell@gmail.com>
2019-03-23 04:04:34 -0400

    change landing text

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -54,7 +54,7 @@ const Landing = () => (
         <Typing.Delay ms={750} />
         <span>Once signed in, </span>
       <Typing.Delay ms={650} />
-      <span>navigate over to Code Talk Chat Rooms and join CODE TALK ONE. </span>
+      <span>you'll be taken to the Code Talk Chat Room. </span>
       <Typing.Delay ms={750} />
       <p>
         <span>There, </span>
