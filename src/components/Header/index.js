57d8f9acb01bce3110af69b8af2ce6a20db533d3
Jeff M <maxjeffwell@gmail.com>
2025-07-29 02:14:03 -0400

    Add responsive top margins to site logo header
    - Implement progressive margin scaling from 10px on mobile to 30px on desktop
    - Use existing breakpoint system for consistent responsive behavior
    - Improve visual balance on larger screens while maintaining mobile compactness
    - Breakpoints: mobile (10px), tablet (15px), laptop (20px), large laptop (25px), desktop (30px)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,27 @@ import { Link } from 'react-router-dom';
 import CodeTalk_Title_Logo from '../Images/Logo/CodeTalk_Title_Logo.png';
 import styled from 'styled-components';
 import LazyImage from '../LazyImage';
+import { breakpoint } from '../Variables';
 
 const StyledHeader = styled.header`
+  margin-top: 10px;
+  
+  @media (min-width: ${breakpoint.tablet}) {
+    margin-top: 15px;
+  }
+  
+  @media (min-width: ${breakpoint.laptop}) {
+    margin-top: 20px;
+  }
+  
+  @media (min-width: ${breakpoint.laptopL}) {
+    margin-top: 25px;
+  }
+  
+  @media (min-width: ${breakpoint.desktop}) {
+    margin-top: 30px;
+  }
+
   .bar {
     display: flex;
     height: 150px;
